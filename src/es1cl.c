f969ebd03046af8477076e56ec6234a117b8573d
Daniel Monteiro <monteiroquiet@gmail.com>
2026-02-27 19:30:28 +0000

    feat: show the name of the invalid or not implemented function upon crashing

--- src/es1cl.c
+++ src/es1cl.c
@@ -6,551 +6,567 @@
 #include <stdio.h>
 #include <GLES/gl.h>
 
-void invalidFunctionInvoked(void)
+
+#define kIntegerPart 16
+
+#define fixToInt(fp)  ((GLfixed)((fp) >> kIntegerPart))
+
+#define intToFix(v)  ((int32_t)((v) << kIntegerPart))
+
+#define Mul(v1, v2) ((GLfixed)((((v1) >> 6) * ((v2) >> 6)) >> 4))
+
+#define Div(v1, v2)  ((GLfixed)((((int64_t) (v1)) * (1 << kIntegerPart)) / (v2)))
+
+#define fixToFloat(fp) (fixToInt(Mul((fp), intToFix(16))) / 16.0f)
+
+
+void invalidFunctionInvoked(char *funcName)
 {
     puts("Only OpenGL ES 1.0 Common Lite is supported");
+    printf("Function called: %s\n", funcName);
     assert(0);
 }
 
-void notImplementedYet(void)
+void notImplementedYet(char *funcName)
 {
     puts("Not implemented yet");
+    printf("Function called: %s\n", funcName);
     assert(0);
 }
 
 GLAPI void APIENTRY glActiveTexture(GLenum texture)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glAlphaFunc(GLenum func, GLclampf ref)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glAlphaFuncx(GLenum func, GLclampx ref)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glBindTexture(GLenum target, GLuint texture)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glBlendFunc(GLenum sfactor, GLenum dfactor)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glClear(GLbitfield mask)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glClearColor(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glClearColorx(GLclampx red, GLclampx green, GLclampx blue, GLclampx alpha)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glClearDepthf(GLclampf depth)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glClearDepthx(GLclampx depth)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glClearStencil(GLint s)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glClientActiveTexture(GLenum texture)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glColor4f(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glColor4x(GLfixed red, GLfixed green, GLfixed blue, GLfixed alpha)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glColorMask(GLboolean red, GLboolean green, GLboolean blue, GLboolean alpha)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glColorPointer(GLint size, GLenum type, GLsizei stride, const GLvoid* pointer)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glCompressedTexImage2D(GLenum target, GLint level, GLenum internalformat, GLsizei width,
                                            GLsizei height, GLint border, GLsizei imageSize, const GLvoid* data)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glCompressedTexSubImage2D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width,
                                               GLsizei height, GLenum format, GLsizei imageSize, const GLvoid* data)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glCopyTexImage2D(GLenum target, GLint level, GLenum internalformat, GLint x, GLint y, GLsizei width,
                                      GLsizei height, GLint border)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glCopyTexSubImage2D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLint x, GLint y,
                                         GLsizei width, GLsizei height)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glCullFace(GLenum mode)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glDeleteTextures(GLsizei n, const GLuint* textures)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glDepthFunc(GLenum func)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glDepthMask(GLboolean flag)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glDepthRangef(GLclampf zNear, GLclampf zFar)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glDepthRangex(GLclampx zNear, GLclampx zFar)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glDisable(GLenum cap)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glDisableClientState(GLenum array)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glDrawArrays(GLenum mode, GLint first, GLsizei count)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glDrawElements(GLenum mode, GLsizei count, GLenum type, const GLvoid* indices)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glEnable(GLenum cap)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glEnableClientState(GLenum array)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glFinish(void)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glFlush(void)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glFogf(GLenum pname, GLfloat param)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glFogfv(GLenum pname, const GLfloat* params)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glFogx(GLenum pname, GLfixed param)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glFogxv(GLenum pname, const GLfixed* params)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glFrontFace(GLenum mode)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glFrustumf(GLfloat left, GLfloat right, GLfloat bottom, GLfloat top, GLfloat zNear, GLfloat zFar)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glFrustumx(GLfixed left, GLfixed right, GLfixed bottom, GLfixed top, GLfixed zNear, GLfixed zFar)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glGenTextures(GLsizei n, GLuint* textures)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI GLenum APIENTRY glGetError(void)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glGetIntegerv(GLenum pname, GLint* params)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI const GLubyte* APIENTRY glGetString(GLenum name)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glHint(GLenum target, GLenum mode)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glLightModelf(GLenum pname, GLfloat param)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glLightModelfv(GLenum pname, const GLfloat* params)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glLightModelx(GLenum pname, GLfixed param)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glLightModelxv(GLenum pname, const GLfixed* params)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glLightf(GLenum light, GLenum pname, GLfloat param)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glLightfv(GLenum light, GLenum pname, const GLfloat* params)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glLightx(GLenum light, GLenum pname, GLfixed param)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glLightxv(GLenum light, GLenum pname, const GLfixed* params)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glLineWidth(GLfloat width)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glLineWidthx(GLfixed width)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glLoadIdentity(void)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glLoadMatrixf(const GLfloat* m)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glLoadMatrixx(const GLfixed* m)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glLogicOp(GLenum opcode)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glMaterialf(GLenum face, GLenum pname, GLfloat param)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glMaterialfv(GLenum face, GLenum pname, const GLfloat* params)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glMaterialx(GLenum face, GLenum pname, GLfixed param)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glMaterialxv(GLenum face, GLenum pname, const GLfixed* params)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glMatrixMode(GLenum mode)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glMultMatrixf(const GLfloat* m)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glMultMatrixx(const GLfixed* m)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glMultiTexCoord4f(GLenum target, GLfloat s, GLfloat t, GLfloat r, GLfloat q)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glMultiTexCoord4x(GLenum target, GLfixed s, GLfixed t, GLfixed r, GLfixed q)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glNormal3f(GLfloat nx, GLfloat ny, GLfloat nz)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glNormal3x(GLfixed nx, GLfixed ny, GLfixed nz)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glNormalPointer(GLenum type, GLsizei stride, const GLvoid* pointer)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glOrthof(GLfloat left, GLfloat right, GLfloat bottom, GLfloat top, GLfloat zNear, GLfloat zFar)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glOrthox(GLfixed left, GLfixed right, GLfixed bottom, GLfixed top, GLfixed zNear, GLfixed zFar)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glPixelStorei(GLenum pname, GLint param)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glPointSize(GLfloat size)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glPointSizex(GLfixed size)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glPolygonOffset(GLfloat factor, GLfloat units)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glPolygonOffsetx(GLfixed factor, GLfixed units)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glPopMatrix(void)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glPushMatrix(void)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glReadPixels(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type,
                                  GLvoid* pixels)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glRotatex(GLfixed angle, GLfixed x, GLfixed y, GLfixed z)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glSampleCoverage(GLclampf value, GLboolean invert)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glSampleCoveragex(GLclampx value, GLboolean invert)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glScalef(GLfloat x, GLfloat y, GLfloat z)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glScalex(GLfixed x, GLfixed y, GLfixed z)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glScissor(GLint x, GLint y, GLsizei width, GLsizei height)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glShadeModel(GLenum mode)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glStencilFunc(GLenum func, GLint ref, GLuint mask)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glStencilMask(GLuint mask)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glStencilOp(GLenum fail, GLenum zfail, GLenum zpass)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glTexCoordPointer(GLint size, GLenum type, GLsizei stride, const GLvoid* pointer)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glTexEnvf(GLenum target, GLenum pname, GLfloat param)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glTexEnvfv(GLenum target, GLenum pname, const GLfloat* params)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glTexEnvx(GLenum target, GLenum pname, GLfixed param)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glTexEnvxv(GLenum target, GLenum pname, const GLfixed* params)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glTexImage2D(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height,
                                  GLint border, GLenum format, GLenum type, const GLvoid* pixels)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glTexParameterf(GLenum target, GLenum pname, GLfloat param)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glTexParameterx(GLenum target, GLenum pname, GLfixed param)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glTexSubImage2D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width,
                                     GLsizei height, GLenum format, GLenum type, const GLvoid* pixels)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glTranslatef(GLfloat x, GLfloat y, GLfloat z)
 {
-    invalidFunctionInvoked();
+    invalidFunctionInvoked(__func__);
 }
 
 GLAPI void APIENTRY glTranslatex(GLfixed x, GLfixed y, GLfixed z)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glVertexPointer(GLint size, GLenum type, GLsizei stride, const GLvoid* pointer)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
 
 GLAPI void APIENTRY glViewport(GLint x, GLint y, GLsizei width, GLsizei height)
 {
-    notImplementedYet();
+    notImplementedYet(__func__);
 }
