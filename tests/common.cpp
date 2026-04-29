4a6504214629057b6b1fc2ee1c1893cc7c6bb51d
Daniel Monteiro <monteiroquiet@gmail.com>
2026-04-29 22:24:42 +0100

    Add infrastructure for unit testing

---
+++ tests/common.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Daniel Monteiro on 27/02/2026.
+//
+#include <stdint.h>
+
+extern "C" {
+#include "internal.h"
+#include <GLES/gl.h>
+}
+
+FramebufferPixelFormat framebuffer[XRES_FRAMEBUFFER * YRES_FRAMEBUFFER];
+
+#ifndef DISABLE_DEPTH_BUFFER
+uint16_t zBuffer[XRES_FRAMEBUFFER * YRES_FRAMEBUFFER];
+#endif
+
+#ifndef DISABLE_STENCIL_BUFFER
+uint8_t stencilBuffer[XRES_FRAMEBUFFER * YRES_FRAMEBUFFER];
+#endif
