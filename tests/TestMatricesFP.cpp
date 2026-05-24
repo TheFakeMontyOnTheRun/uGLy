4da0ad84b378522221fee0a4e7442585e3989848
Daniel Monteiro <monteiroquiet@gmail.com>
2026-05-24 21:33:29 +0100

    new test: Multiplying 4x4 matrix by it's inverse should yield identity

--- tests/TestMatricesFP.cpp
+++ tests/TestMatricesFP.cpp
@@ -14,6 +14,46 @@ extern "C" {
 #include "GLES/gl.h"
 }
 
+TEST(TestMatricesFP, multiplicationWithInverseMatrixProducesIdentity)
+{
+    GLfixed mat[16];
+    GLfixed inverse[16];
+    GLfixed ident[16];
+    GLfixed result[16];
+
+    mat4x4_identity(&ident[0]);
+
+    memset(&mat[0], 0, sizeof(GLfixed) * 16);
+    memset(&inverse[0], 0, sizeof(GLfixed) * 16);
+    memset(&result[0], 0, sizeof(GLfixed) * 16);
+    mat[0] = intToFix(1);
+    mat[1] = intToFix(2);
+    mat[4] = intToFix(3);
+    mat[5] = intToFix(4);
+
+    mat[10] = intToFix(1);
+    mat[11] = intToFix(2);
+    mat[14] = intToFix(3);
+    mat[15] = intToFix(4);
+
+    inverse[0] = -intToFix(2);
+    inverse[1] = intToFix(1);
+    inverse[4] = Div(intToFix(3), intToFix(2));
+    inverse[5] = -Div(intToFix(1), intToFix(2));
+
+    inverse[10] = -intToFix(2);
+    inverse[11] = intToFix(1);
+    inverse[14] = Div(intToFix(3), intToFix(2));
+    inverse[15] = -Div(intToFix(1), intToFix(2));
+
+    mat4x4_mul(&mat[0], &inverse[0], &result[0]);
+
+    for (int c = 0; c < 16; ++c)
+    {
+        ASSERT_EQ(result[c], ident[c]);
+    }
+}
+
 TEST(TestMatricesFP, multiplicationWithIdentityResultsInTheMatrix)
 {
     GLfixed mat[16];
