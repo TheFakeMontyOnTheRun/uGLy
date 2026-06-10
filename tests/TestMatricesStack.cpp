3b08a357e75f1f738bbffd7e5dc2c6688c0f75cb
Daniel Monteiro <monteiroquiet@gmail.com>
2026-06-10 22:14:35 +0100

    Improve matrix stack tests

--- tests/TestMatricesStack.cpp
+++ tests/TestMatricesStack.cpp
@@ -3,16 +3,34 @@
 //
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <assert.h>
-#include <stdint.h>
-
-#include "internal.h"
+#include <cassert>
+#include <cstdint>
 
 extern "C" {
+#include "internal.h"
 #include "GLES/gl.h"
 }
 
-TEST(TestMatricesStack, canPushMatrices)
+class TestMatricesStack : public testing::Test
+{
+protected:
+    void SetUp() override {
+        uGLyInit();
+    }
+
+    void TearDown() override {
+
+    }
+};
+
+TEST_F(TestMatricesStack, canPushMatrices)
 {
     glPushMatrix();
+    ASSERT_EQ( glGetError(), GL_NO_ERROR);
+}
+
+TEST_F(TestMatricesStack, cantPopMatrixIf)
+{
+    glPopMatrix();
+    ASSERT_EQ( glGetError(), GL_STACK_UNDERFLOW);
 }
